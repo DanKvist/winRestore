9f007c5c24884870cf3d8e1a4efefcc0bee19e98
Dan Thilderkvist <dan.thilderkvist@hotmail.com>
2022-01-28 17:05:35 +0100

    Window creation, basic monitor info.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
 
+void OnSize(HWND hwnd, UINT flag, int width, int height);
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPAram);
 
-int main() {
+
+const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevINstance, LPSTR lpCmdLine, int cmdShow) {
     std::ofstream logFile;
     
     logFile.open("log.txt");
@@ -12,9 +21,44 @@ int main() {
         return 1;
     }
 
-    while (true) {
-        std::cout << "Hello sir!" << std::endl;
-        Sleep(2000);
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"Learn to Program Windows",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+        );
+
+    if (hwnd == NULL)
+        return 1;
+
+    int monitorCount = GetSystemMetrics(SM_CMONITORS);
+    int primaryWidth = GetSystemMetrics(SM_CXFULLSCREEN);
+    int primaryHeight = GetSystemMetrics(SM_CYFULLSCREEN);
+    std::cout << "There are " << monitorCount << " monitors connected." << std::endl;
+    std::cout << "Primary resolution is: " << primaryWidth << " x " << primaryHeight << std::endl;
+
+    ShowWindow(hwnd, cmdShow);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+//        std::cout << "Hello sir!" << std::endl;
+//        Sleep(2000);
+
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+
     }
         
 
@@ -22,4 +66,59 @@ int main() {
 
     logFile.close();
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_SIZE:
+        {
+            int width = LOWORD(lParam);
+            int height = HIWORD(lParam);
+
+            OnSize(hwnd, (UINT)wParam, width, height);
+        }
+        return 0;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+2));
+
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+
+    case WM_POWERBROADCAST:
+        if (wParam == PBT_APMSUSPEND) 
+        {
+            std::cout << "Suspended" << std::endl;
+        } else if (wParam == PBT_APMRESUMEAUTOMATIC) {
+            std::cout << "Resumed" << std::endl;
+        } 
+        return 0;
+
+    case WM_CLOSE:
+        if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
+        {
+            DestroyWindow(hwnd);
+        }
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    default:
+        {
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }        
+    }
+}
+
+void OnSize(HWND hwnd, UINT flag, int width, int height)
+{
+    //Do stuff
 }
