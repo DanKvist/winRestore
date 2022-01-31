e218526f865bb177d1a1c6286f041575af2c8d82
Dan Thilderkvist <dan.thilderkvist@hotmail.com>
2022-01-31 14:20:20 +0100

    Detection update
    - Detection of screen changes
    - Detection of saved setting
    - Containers for saved settings and saved window positions

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@
 
 #include <iostream>
 #include <fstream>
+#include <cstring>
 #include <vector>
 #include <Windows.h>
 
 // Forward declarations
 void OnSize(HWND hwnd, UINT flag, int width, int height);
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPAram);
-BOOL CALLBACK myInfoEnumProc(HMONITOR hMonitor, HDC hdc, LPRECT rect, LPARAM data);
+BOOL CALLBACK myMonitorEnumProc(HMONITOR hMonitor, HDC hdc, LPRECT rect, LPARAM data);
 BOOL CALLBACK myEnumWindowProc(HWND hwnd, LPARAM data);
+bool CompareMonitorSetup(const MONITORINFOEX *monitor1, const MONITORINFOEX *monitor2);
 void RestoreWindows();
 
 
@@ -74,6 +76,7 @@ public:
 // Global variables
 const wchar_t CLASS_NAME[] = L"Sample Window Class";
 std::vector<WindowData> windowDataList;
+std::vector<MONITORINFOEX> savedMonitorSetup;
 
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevINstance, LPSTR lpCmdLine, int cmdShow) {
@@ -108,17 +111,46 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevINstance, LPSTR lpCmdLine, int c
     if (hwnd == NULL)
         return 1;
 
-
+    // Grab monitor stuff
     std::cout << std::endl << "Display info:" << std::endl;
-    EnumDisplayMonitors(NULL, NULL, &myInfoEnumProc, NULL);
+    EnumDisplayMonitors(NULL, NULL, &myMonitorEnumProc, reinterpret_cast<LPARAM>(&savedMonitorSetup));
 
+    for (int idx = 0; idx < savedMonitorSetup.size(); idx++)
+    {
+        if (savedMonitorSetup[idx].dwFlags & MONITORINFOF_PRIMARY) 
+            printf("Primary\n");
+
+        printf("Device name is: %S\n", savedMonitorSetup[idx].szDevice);
+
+        LONG width = savedMonitorSetup[idx].rcMonitor.right - savedMonitorSetup[idx].rcMonitor.left;
+        LONG height = savedMonitorSetup[idx].rcMonitor.bottom - savedMonitorSetup[idx].rcMonitor.top;
+        printf("Device resolution is: %i x %i\n\n", width, height);
+    }
+
+    // Test monitor compare
+    std::cout << std::endl << "Comparing: " << std::endl;
+    std::vector<MONITORINFOEX> tempMonitorSetup;
+    EnumDisplayMonitors(NULL, NULL, &myMonitorEnumProc, reinterpret_cast<LPARAM>(&tempMonitorSetup));
+
+    for (int idx = 0; idx < tempMonitorSetup.size(); idx++)
+    {
+        if (CompareMonitorSetup(&savedMonitorSetup[idx], &tempMonitorSetup[idx])) 
+            printf("Same rectangles\n");
+            printf("Upper left: (%i, %i), Lower right: (%i, %i)\n", 
+                                    savedMonitorSetup[idx].rcMonitor.left,
+                                    savedMonitorSetup[idx].rcMonitor.top,
+                                    savedMonitorSetup[idx].rcMonitor.right,
+                                    savedMonitorSetup[idx].rcMonitor.bottom);
+    }
+    
+    // Grab window stuff
     std::cout << std::endl << "Window info:" << std::endl;
     EnumDesktopWindows(NULL, &myEnumWindowProc, reinterpret_cast<LPARAM>(&windowDataList));
 
-    for (int idx = 0; idx < windowDataList.size(); idx++)
-    {
-        windowDataList[idx].DumpWindowInfo();
-    }
+    // for (int idx = 0; idx < windowDataList.size(); idx++)
+    // {
+    //     windowDataList[idx].DumpWindowInfo();
+    // }
 
 
     ShowWindow(hwnd, cmdShow);
@@ -173,16 +205,29 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
     case WM_DISPLAYCHANGE:
         {
-            int bitsPerPix = (UINT)wParam;
+            std::vector<MONITORINFOEX> tempMonitorSetup;
+            EnumDisplayMonitors(NULL, NULL, &myMonitorEnumProc, reinterpret_cast<LPARAM>(&tempMonitorSetup));
+
             int width = LOWORD(lParam);
             int height = HIWORD(lParam);
             std::cout << "Display change message" << std::endl;
-            std::cout << "New resolution: " << width << " x " << height << std::endl << std::endl;
 
-            if ((width == 2560) && (height == 1440))
+            if (savedMonitorSetup.size() == tempMonitorSetup.size())
             {
-                std::cout << "Original resolution" << std::endl;
-                RestoreWindows();
+                std::cout << "Same monitor count" << std::endl;
+
+                bool sameSettings = true;
+                for (int idx = 0; idx < savedMonitorSetup.size(); idx++)
+                {
+                    if (!CompareMonitorSetup(&savedMonitorSetup[idx], &tempMonitorSetup[idx]))
+                        sameSettings = false;
+                }
+
+                if (sameSettings)
+                {
+                    std::cout << "Original resolution" << std::endl;
+                    RestoreWindows();
+                }                
             }
         }
         return 1;
@@ -204,20 +249,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     }
 }
 
-BOOL CALLBACK myInfoEnumProc(HMONITOR hMonitor, HDC hdc, LPRECT rect, LPARAM data)
+BOOL CALLBACK myMonitorEnumProc(HMONITOR hMonitor, HDC hdc, LPRECT rect, LPARAM lParam)
 {
+    std::vector<MONITORINFOEX>* pMonitors = reinterpret_cast<std::vector<MONITORINFOEX>*>(lParam);
     MONITORINFOEX iMonitor;
     iMonitor.cbSize = sizeof(MONITORINFOEX);
     GetMonitorInfo(hMonitor, &iMonitor);
 
-    if (iMonitor.dwFlags & MONITORINFOF_PRIMARY) 
-        printf("Primary\n");
+    pMonitors->push_back(iMonitor);
 
-    printf("Device name is: %S\n", iMonitor.szDevice);
-
-    LONG width = rect->right - rect->left;
-    LONG height = rect->bottom - rect->top;
-    printf("Device resolution is: %i x %i\n\n", width, height);
     return 1;
 }
 
@@ -241,9 +281,17 @@ BOOL CALLBACK myEnumWindowProc(HWND hwnd, LPARAM lParam)
 void RestoreWindows() 
 {
     for (int idx = 0; idx < windowDataList.size(); idx++)
-    {        
+    {
         windowDataList[idx].RestoreWindow();
     }
+}
+
+bool CompareMonitorSetup(const MONITORINFOEX *monitor1, const MONITORINFOEX *monitor2)
+{
+    return (    EqualRect(&monitor1->rcMonitor, &monitor2->rcMonitor)
+                && (monitor1->dwFlags == monitor2->dwFlags)
+                && !wcscmp(monitor1->szDevice, monitor2->szDevice)
+                );
 }
 
 
